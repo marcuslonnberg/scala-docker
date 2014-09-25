5ec77f279b7a716b77c652ba55dddb244bb89f6e
Marcus Lönnberg <marcus@lonnberg.nu>
2014-09-25 21:30:08 +0200

    Add tests for container APIs

---
+++ remote-api/src/test/scala/se/marcuslonnberg/scaladocker/remote/api/ContainerApiSpec.scala
@@ -0,0 +1,41 @@
+package se.marcuslonnberg.scaladocker.remote.api
+
+import akka.actor.ActorSystem
+import akka.stream.MaterializerSettings
+import akka.stream.scaladsl2.FlowMaterializer
+import akka.testkit.TestKit
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.{FlatSpecLike, Inspectors, Matchers}
+import se.marcuslonnberg.scaladocker.remote.models.{ContainerConfig, HostConfig, ImageName}
+
+class ContainerApiSpec extends TestKit(ActorSystem("container-api")) with FlatSpecLike with Matchers with ScalaFutures with IntegrationPatience with Inspectors {
+  implicit val mat = FlowMaterializer(MaterializerSettings(system))
+
+  val client = DockerClient()
+
+  val busybox = ImageName("busybox")
+
+  "Container API" should "list all containers" in {
+    val containerId = client.runLocal(ContainerConfig(busybox, cmd = List("ls", "/")), HostConfig()).futureValue
+
+    val containers = client.containers.list(all = true).futureValue
+
+    forAtLeast(1, containers) { container =>
+      container.id shouldEqual containerId
+      container.image shouldEqual busybox
+      container.command shouldEqual "ls /"
+    }
+  }
+
+  it should "get info about a container" in {
+    val containerId = client.runLocal(ContainerConfig(busybox, cmd = List("ls", "/")), HostConfig()).futureValue
+
+    val container = client.containers.get(containerId).futureValue
+
+    container.id shouldEqual containerId
+    container.path shouldEqual "ls"
+    container.args should contain theSameElementsInOrderAs Seq("/")
+
+    container.config.image shouldEqual busybox
+  }
+}
