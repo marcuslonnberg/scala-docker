b99301562444f81e15189c99decad4a0accc82e1
Marcus Lönnberg <marcus@lonnberg.nu>
2015-01-21 19:52:25 +0100

    Use latest dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,14 +1,14 @@
 import sbt._
 
 object Dependencies {
-  val akkaVersion = "2.3.6"
+  val akkaVersion = "2.3.9"
   val akkaHttpStreamsVersion = "1.0-M2"
-  val json4sVersion = "3.2.10"
+  val json4sVersion = "3.2.11"
 
   val json4sNative = "org.json4s" %% "json4s-native" % json4sVersion
   val json4sExt = "org.json4s" %% "json4s-ext" % json4sVersion
 
-  val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "1.4.0"
+  val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "1.6.0"
 
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
   val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test"
@@ -17,9 +17,9 @@ object Dependencies {
 
   val jtar = "org.kamranzafar" % "jtar" % "2.2"
 
-  val commonsCodec = "commons-codec" % "commons-codec" % "1.9"
+  val commonsCodec = "commons-codec" % "commons-codec" % "1.10"
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.2" % "test"
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.3" % "test"
 
   val all = Seq(
     akkaActor, akkaStream, akkaHttp, jtar, commonsCodec,
