e0d08b375c76211428cf217c4522a64b9fcd5d13
Marcus Lönnberg <marcus@lonnberg.nu>
2015-03-26 15:45:54 +0100

    Reorganize dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,26 +3,28 @@ import sbt._
 object Dependencies {
   val akkaVersion = "2.3.9"
   val akkaHttpStreamsVersion = "1.0-M3"
-  val json4sVersion = "3.2.11"
 
-  val json4sNative = "org.json4s" %% "json4s-native" % json4sVersion
-  val json4sExt = "org.json4s" %% "json4s-ext" % json4sVersion
-
-  val nscalaTime = "com.github.nscala-time" %% "nscala-time" % "1.8.0"
-
-  val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
-  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test"
-  val akkaStream = "com.typesafe.akka" %% "akka-stream-experimental" % akkaHttpStreamsVersion
-  val akkaHttp = "com.typesafe.akka" %% "akka-http-experimental" % akkaHttpStreamsVersion
-
-  val jtar = "org.kamranzafar" % "jtar" % "2.2"
-
-  val commonsCodec = "commons-codec" % "commons-codec" % "1.10"
-
-  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
-
-  val all = Seq(
-    akkaActor, akkaStream, akkaHttp, jtar, commonsCodec,
-    json4sNative, json4sExt, nscalaTime, scalaTest, akkaTestkit
+  val akka = Seq(
+    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+    "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test",
+    "com.typesafe.akka" %% "akka-stream-experimental" % akkaHttpStreamsVersion,
+    "com.typesafe.akka" %% "akka-http-experimental" % akkaHttpStreamsVersion
   )
+
+  val json = Seq(
+    "org.json4s" %% "json4s-native" % "3.2.11",
+    "org.json4s" %% "json4s-ext" % "3.2.11"
+  )
+
+  val misc = Seq(
+    "org.kamranzafar" % "jtar" % "2.2",
+    "commons-codec" % "commons-codec" % "1.10",
+    "com.github.nscala-time" %% "nscala-time" % "1.8.0",
+    "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+  )
+
+  val all =
+    akka ++
+      json ++
+      misc
 }
