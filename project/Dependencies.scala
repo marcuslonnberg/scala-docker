b84083b870d36f8e9e111012f722e52bfe4d5f4b
Marcus Lönnberg <marcus@lonnberg.nu>
2015-05-11 17:42:50 +0200

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val akkaVersion = "2.3.10"
-  val akkaHttpStreamsVersion = "1.0-RC1"
+  val akkaHttpStreamsVersion = "1.0-RC2"
 
   val akka = Seq(
     "com.typesafe.akka" %% "akka-actor" % akkaVersion,
@@ -12,13 +12,13 @@ object Dependencies {
   )
 
   val json = Seq(
-    "com.typesafe.play" %% "play-json" % "2.3.8"
+    "com.typesafe.play" %% "play-json" % "2.3.9"
   )
 
   val misc = Seq(
     "org.kamranzafar" % "jtar" % "2.2",
     "commons-codec" % "commons-codec" % "1.10",
-    "com.github.nscala-time" %% "nscala-time" % "1.8.0",
+    "com.github.nscala-time" %% "nscala-time" % "2.0.0",
     "org.scalatest" %% "scalatest" % "2.2.4" % "test"
   )
 
